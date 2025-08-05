0b242d032729b4dd0ebe0ed06f2f7ac2e83ce6ff
DevasaniNaveen <devasaninawin@gmail.com>
2025-08-05 17:52:25 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,19 +1,11 @@
-import com.sun.net.httpserver.HttpServer;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
+package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", (exchange -> {
-            String response = "Hello from simple Java HTTP server!";
-            exchange.sendResponseHeaders(200, response.length());
-            OutputStream os = exchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
-        }));
-        server.start();
-        System.out.println("Server started on port 8080");
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
